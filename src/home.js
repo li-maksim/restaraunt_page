66c24564e866b31059cb2e323ebe15b8c94eccbb
li-maksim <max.mail4@yandex.ru>
2024-10-03 20:14:59 +0300

    Added content to Home and Menu tabs

--- src/home.js
+++ src/home.js
@@ -1,3 +1,8 @@
+import home1 from "./img/home1.jpg";
+import home2 from "./img/home2.jpg";
+import home3 from "./img/home3.jpeg";
+import home4 from "./img/home4.jpg";
+
 export const HomeTab = (() => {
     const content = document.querySelector('#content');
     const home = document.createElement('div');
@@ -5,7 +10,7 @@ export const HomeTab = (() => {
     
     const h2 = document.createElement('h2');
     h2.setAttribute('class', 'h2');
-    h2.textContent = 'Home';
+    h2.textContent = "Welcome to Bob's Burgers!";
 
     const homeContent = document.createElement('div');
     homeContent.setAttribute('class', 'home_content');
@@ -13,16 +18,27 @@ export const HomeTab = (() => {
     const descr = document.createElement('div');
     descr.setAttribute('class', 'text');
     descr.setAttribute('class', 'home_descr');
+    descr.textContent = "Bob's Burgers carries on the tradition of Mouthwatering Hamburgers made from Choice Beef, Ground Fresh Daily.  Other favorite menu selections include our award winning Pork Baby Back Ribs, The Mammoth Burger for hearty appetites, our Durham Ranch Bison Burger, and a variety of sandwiches and salads with homemade dressings and sauces to top everything off. Welcome!"
 
+    const hr = document.createElement('hr');
+
+    let homeImgs = [];
     const imgs = document.createElement('div');
     imgs.setAttribute('class', 'imgs_wrapper');
     for (let i = 0; i <= 3; i++) {
         const img = document.createElement('img');
         img.setAttribute('class', 'home_img');
         imgs.appendChild(img);
+        homeImgs.push(img);
     };
 
+    homeImgs[0].src = home1;
+    homeImgs[1].src = home2;
+    homeImgs[2].src = home3;
+    homeImgs[3].src = home4;
+
     homeContent.appendChild(descr);
+    homeContent.appendChild(hr);
     homeContent.appendChild(imgs);
     home.appendChild(h2);
     home.appendChild(homeContent);
--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,14 @@
+import menu1 from "./img/menu1.jpg";
+import menu2 from "./img/menu2.jpg";
+import menu3 from "./img/menu3.jpg";
+import menu4 from "./img/menu4.jpg";
+import menu5 from "./img/menu5.jpeg";
+import menu6 from "./img/menu6.jpg";
+
 export const MenuTab = (() => {
     const content = document.querySelector('#content');
     const menu = document.createElement('div');
-    home.setAttribute('class', 'menu_tab');
+    menu.setAttribute('class', 'menu_tab');
     
     const h2 = document.createElement('h2');
     h2.setAttribute('class', 'h2');
@@ -11,7 +18,7 @@ export const MenuTab = (() => {
     menuContent.setAttribute('class', 'menu_content');
 
     let menuCards = [];
-    for (let i = 0; i <= 6; i++) {
+    for (let i = 0; i <= 5; i++) {
         const MenuObj = {};
         const menuCard = document.createElement('div');
         menuCard.setAttribute('class', 'menu_card');
@@ -23,13 +30,46 @@ export const MenuTab = (() => {
         MenuObj.descr = document.createElement('div');
         MenuObj.descr.setAttribute('class', 'menu_descr');
         MenuObj.descr.setAttribute('class', 'text');
+        MenuObj.price = document.createElement('div');
+        MenuObj.price.setAttribute('class', 'price');
 
         menuCard.appendChild(MenuObj.img);
         menuCard.appendChild(MenuObj.h3);
         menuCard.appendChild(MenuObj.descr);
+        menuCard.appendChild(MenuObj.price);
         menuContent.appendChild(menuCard);
         menuCards.push(MenuObj);
     };
+
+    menuCards[0].img.src = menu1;
+    menuCards[0].h3.textContent = 'Pork Baby Back Ribs Burger';
+    menuCards[0].descr.textContent = 'New! Pork ribs, onions, pickles, barbecue sauce and a secret ingridient.';
+    menuCards[0].price.textContent = '$6.9';
+
+    menuCards[1].img.src = menu2;
+    menuCards[1].h3.textContent = 'Mammoth Burger';
+    menuCards[1].descr.textContent = 'Extra big burger! Double beef patty, onion rings, lettuce and special sauce.';
+    menuCards[1].price.textContent = '$8.8';
+
+    menuCards[2].img.src = menu3;
+    menuCards[2].h3.textContent = 'Durham Ranch Bison Burge';
+    menuCards[2].descr.textContent = 'Hit! Beef patty, fresh tomatoes and onions, ranch sauce.';
+    menuCards[2].price.textContent = '$7.2';
+
+    menuCards[3].img.src = menu4;
+    menuCards[3].h3.textContent = 'Super Cheeseburger';
+    menuCards[3].descr.textContent = 'Hit! Beef, cheese, salad, cheese, onions, cheese and cheese again.';
+    menuCards[3].price.textContent = '$6.9';
+
+    menuCards[4].img.src = menu5;
+    menuCards[4].h3.textContent = 'Humble chickenburger';
+    menuCards[4].descr.textContent = 'Light burger! Chicken, tomatoes and a tasty sauce';
+    menuCards[4].price.textContent = '$5.3';
+
+    menuCards[5].img.src = menu6;
+    menuCards[5].h3.textContent = 'Pizza Burger';
+    menuCards[5].descr.textContent = 'Pizza + Burger!!! Bacon. beef patty, cheese, tomatoes, special sauce';
+    menuCards[5].price.textContent = '$9.69';
 
     menu.appendChild(h2);
     menu.appendChild(menuContent);
