61851794338677fba07ed5d47b61f6385deb83d2
li-maksim <max.mail4@yandex.ru>
2024-10-03 20:36:22 +0300

    Added function to switch tabs

--- src/index.js
+++ src/index.js
@@ -2,4 +2,44 @@ import "./style.css";
 import { HomeTab } from "./home";
 import { MenuTab } from "./menu";
 
-MenuTab();
+const Buttons = (() => {
+
+    const SwitchTabs = (() => {
+        function hideTab() {
+            const content = document.querySelector('#content');
+            content.textContent = '';
+        };
+    
+        function home() {
+            hideTab();
+            HomeTab();
+        };
+    
+        function menu() {
+            hideTab();
+            MenuTab();
+        };
+    
+        return {home, menu}
+    })();
+
+    function rmClass() {
+        const buttons = document.querySelectorAll('.tab');
+        buttons.forEach((el) => el.classList.remove('act_btn'));
+    };
+    function activate() {
+        this.classList.add('act_btn');
+    };
+
+    const homeBtn = document.querySelector('#home');
+    homeBtn.addEventListener('click', rmClass);
+    homeBtn.addEventListener('click', activate);
+    homeBtn.addEventListener('click', SwitchTabs.home);
+    const menuBtn = document.querySelector('#menu');
+    menuBtn.addEventListener('click', rmClass);
+    menuBtn.addEventListener('click', activate);
+    menuBtn.addEventListener('click', SwitchTabs.menu);
+
+})();
+
+HomeTab();
